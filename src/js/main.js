55686cb753903d6b2c0fe47805a21450022c846b
Igor <maxejj@gmail.com>
2021-12-11 19:25:26 +0200

    гзв

--- src/js/main.js
+++ src/js/main.js
@@ -8,10 +8,12 @@ $('.calc-block form input').on('keyup',function(){
         var element = $(this);
         if (element.val() == "") {
             $('.calc-block form button').removeClass('active');
+            $('.calc-block form button').prop( "disabled", true);
 
         }
         else {
             $('.calc-block form button').addClass('active');
+            $('.calc-block form button').prop( "disabled", false );
         }
      });
     
@@ -24,4 +26,23 @@ $('.add-to-fav').on('click',function(){
 
 $('.fav-status').on('click',function(){
     $(this).toggleClass('active');
+})
+
+$('.plenka').on('click',function(){
+    $(this).parent().fadeOut();
+})
+
+$('.search-button').on('click',function(){
+   $('.search-popup').fadeIn();
+})
+
+$('.burger').on('click',function(){
+    $(this).toggleClass('active');
+    $('body').toggleClass('hiddy');
+    $('.mobile-menu').toggleClass('active');
+})
+
+$('.droppy-action').on('click',function(){
+    $(this).toggleClass('active');
+    $(this).closest('.with-drop').find('.droppy').slideToggle();
 })
